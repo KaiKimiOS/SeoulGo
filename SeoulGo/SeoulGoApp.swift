ebef7e37321a4f9f941d28b28f3016080f962d31
김형준 <sdffs310@naver.com>
2023-12-30 00:21:52 +0900

    Initial Commit

---
+++ SeoulGo/SeoulGoApp.swift
@@ -0,0 +1,32 @@
+//
+//  SeoulGoApp.swift
+//  SeoulGo
+//
+//  Created by kaikim on 12/30/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SeoulGoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SeoulGoUITests/SeoulGoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SeoulGoUITestsLaunchTests.swift
+//  SeoulGoUITests
+//
+//  Created by kaikim on 12/30/23.
+//
+
+import XCTest
+
+final class SeoulGoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
