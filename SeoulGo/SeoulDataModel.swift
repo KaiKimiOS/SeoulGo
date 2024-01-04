ebd0bb4fe5dadf62f90c413ff15cfcca3ce3375a
김형준 <sdffs310@naver.com>
2024-01-04 15:02:55 +0900

    feat: Json에 맞는 모델 구현 완료
    더미데이터 화면에 보여주기 구현

--- SeoulGo/SeoulDataModel.swift
+++ SeoulGo/SeoulDataModel.swift
@@ -8,33 +8,73 @@
 import Foundation
 
 
-struct SeoulDataModel: Decodable {
+struct SeoulDataModel:Decodable {
+    
+    let ListPublicReservationSport:ListPublicReservationSport
+}
+
+struct ListPublicReservationSport:Decodable {
     
     let listTotalCount:Int
-    let resultCode:String
-    let resultMessage:String
-    let areaName:String
-    let playName:String
-    let informationURL:String
-
+    let result:Result
+    let resultDetails:[Row]
+    
     enum CodingKeys:String, CodingKey {
-                case listTotalCount = "list_total_count"
-                case resultCode = "RESULT.CODE"
-//                case serviceID = "SVCID"
-                case resultMessage = "RESULT.MESSAGE"
-//                case gubun = "GUBUN"
-//                case maxClass = "MAXCLASSNM"
-//                case minClass = "MINCLASSNM"
-//                case serviceStatus = "SVCSTATNM"
-//                case serviceName = "SVCNM"
-//                case payment = "PAYATNM"
-                case playName = "PLACENM"
-//                case useTarger = "USETGTINFO"
-                case informationURL = "SVCURL"
-                case areaName = "AREANM"
-//                case serviceStartDate = "SVCOPNBGNDT"
-//                case serviceEndDate = "SVCOPNENDDT"
-//                case registerStartDate = "RCPTBGNDT"
-//                case registerEndDate = "RCPTENDDT"
+        case listTotalCount = "list_total_count"
+        case result = "RESULT"
+        case resultDetails = "row"
+    }
+}
+
+struct Result:Decodable {
+    let code: String
+    let message:String
+    
+    enum CodingKeys: String, CodingKey {
+        case code = "CODE"
+        case message = "MESSAGE"
+    }
+}
+
+struct Row: Decodable {
+    
+    let gubun,
+        serviceID,
+        maxClass,
+        minClass,
+        serviceStatus,
+        serviceName,
+        payment,
+        placeName,
+        userTarget,
+        informationURL,
+        locationX,
+        locationY,
+        serviceStartDate,
+        serviceEndDate,
+        registerStartDate,
+        registerEndDate,
+        areaName,
+        imageURL:String
+    
+    enum CodingKeys: String, CodingKey {
+        case gubun = "GUBUN"
+        case serviceID = "SVCID"
+        case maxClass = "MAXCLASSNM"
+        case minClass = "MINCLASSNM"
+        case serviceStatus = "SVCSTATNM"
+        case serviceName = "SVCNM"
+        case payment = "PAYATNM"
+        case placeName = "PLACENM"
+        case userTarget = "USETGTINFO"
+        case informationURL = "SVCURL"
+        case locationX = "X"
+        case locationY = "Y"
+        case serviceStartDate = "SVCOPNBGNDT"
+        case serviceEndDate = "SVCOPNENDDT"
+        case registerStartDate = "RCPTBGNDT"
+        case registerEndDate = "RCPTENDDT"
+        case areaName = "AREANM"
+        case imageURL = "IMGURL"
     }
 }
